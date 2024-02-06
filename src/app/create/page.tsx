a3aba0434edd5721a125061ca2664aa3a59b53d8
oleveloper <oleveloper@gmail.com>
2024-02-06 14:44:12 +0900

    Add writing features

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,33 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 export default function Create() {
+    const router = useRouter();
+    // @ts-ignore
+    async function submit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const body = form.body.value;
+        const resp = await fetch('http://localhost:9999/page', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({title, body})
+        });
+
+        const result = await resp.json();
+        router.push('/read/' + result.id);
+    }
+    
     return (
     <div>
         <h2>Create</h2>
+        <form onSubmit={submit}>
+            <p><input type="text" placeholder="title" name="title"/></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="Create"/></p>
+        </form>
     </div>)
 }
